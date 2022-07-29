2a3ab1a01b56cd33001aee9d1b3a254af5e56a3b
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2022-07-29 12:56:02 -0400

    fetching Events from DB (WIP)

--- app/src/main/java/com/example/b07project/Event.java
+++ app/src/main/java/com/example/b07project/Event.java
@@ -2,27 +2,41 @@ package com.example.b07project;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public class Event {
 
-    String creator;
-    String endTime;
-    String eventID;
-    ArrayList<User> customers;
-    String startTime;
-    String venueID;
+    public String creator;
+    public String endTime;
+    public String eventID;
+    private ArrayList<User> userCustomers;
+    public ArrayList<String> customers;
+    public String startTime;
+    public String venueID;
+    public int capacity;
 
 
     //TODO: Check what type we want each field to be and initialize an Event
-    public Event(String creator, String startTime, String endTime, String eventID, String venueID){
-
+    public Event(String creator, String startTime, String endTime, String eventID, String venueID, int capacity, ArrayList<String> customers){
+        this.creator = creator;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.eventID = eventID;
+        this.venueID = venueID;
+        this.capacity = capacity;
+        this.customers = customers;
+        //TODO: find customers with given usernames, and add them to this.userCustomers
     }
 
     public void addCustomer(User player){
-        customers.add(player);
+
+        customers.add(player.username);
+        userCustomers.add(player);
     }
 
     public ArrayList<User> fetchCustomers(){
-        return customers;
+        return userCustomers;
     }
+
+
 }
--- app/src/main/java/com/example/b07project/User.java
+++ app/src/main/java/com/example/b07project/User.java
@@ -1,22 +1,52 @@
 package com.example.b07project;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 
-//Base class that can be inherited by both Admin and Customer
+import androidx.annotation.NonNull;
+
+//Base class to communicate with database that can be inherited by both Admin and Customer
 public abstract class User {
     String username;
+    static ArrayList<Event> allEvents = new ArrayList<Event>();
+    static ArrayList<Venue> allVenues = new ArrayList<Venue>();
 
-
-    //TODO
-    //Talks directly to database to fetch events
-    ArrayList<Event> fetchAllEvents(){
-        return new ArrayList<Event>();
+    static ArrayList<Event> fetchAllEvents(){
+        return allEvents;
     }
 
-    //TODO
-    ArrayList<Venue> fetchAllVenues(){
-        return new ArrayList<Venue>();
+    static ArrayList<Venue> fetchAllVenues(){
+        return allVenues;
     }
+
+    //Initialize data connection
+    static void initialize(){
+
+        //Initialise synchronization between allEvents and Firebase
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://b07project-696e9-default-rtdb.firebaseio.com/").getReference("events");
+        ArrayList<Event> events = new ArrayList<Event>();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot child: snapshot.getChildren()){
+                    Event event = child.getValue(Event.class);
+                    System.out.println(event);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 
 }
