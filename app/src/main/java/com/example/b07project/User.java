517b1687dac38d5da6f036b615c16ca1e2cf6b52
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2022-07-29 20:52:36 -0400

    Added realtime data updates on changes in child data

--- app/src/main/java/com/example/b07project/User.java
+++ app/src/main/java/com/example/b07project/User.java
@@ -2,15 +2,14 @@ package com.example.b07project;
 
 import android.util.Log;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 
 import androidx.annotation.NonNull;
 
@@ -59,20 +58,52 @@ public abstract class User {
 //        ref.child("customers").child(testCustomer.username).setValue(testCustomer);
 //        ref.child("events").child(testEvent.eventID).setValue(testEvent);
 
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot child: snapshot.getChildren()){
+//                    Customer customer = child.getValue(Customer.class);
+//                    if (!allCustomers.contains(customer)){
+//                        allCustomers.add(customer);
+//                    }
+//                    Log.d("test", customer.username);
+//                    Log.d("test", customer.fullName);
+//                }
+//            }
+
         //Initialise synchronization between allCustomers and Firebase
         ref = fire.getReference("customers");
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot child: snapshot.getChildren()){
-                    Customer customer = child.getValue(Customer.class);
-                    if (!allCustomers.contains(customer)){
-                        allCustomers.add(customer);
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey){
+                Customer customer = dataSnapshot.getValue(Customer.class);
+                if (!allCustomers.contains(customer)){
+                    allCustomers.add(customer);
+                }
+                Log.d("test", "Found customer: " + customer.username);
+//                Log.d("test", customer.fullName);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+                Customer customer = dataSnapshot.getValue(Customer.class);
+                for (User c : User.fetchAllCustomers()) {
+                    if (c.username == customer.username) {
+                        Customer cu = (Customer)c;
+                        c.password = customer.password;
+                        cu.fullName = customer.fullName;
+                        cu.joinedEvents = customer.joinedEvents;
+                        cu.scheduledEvents = customer.scheduledEvents;
+                        break;
                     }
-                    Log.d("test", customer.username);
-                    Log.d("test", customer.fullName);
                 }
             }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Customer customer = dataSnapshot.getValue(Customer.class);
+                allCustomers.remove(customer);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -82,17 +113,36 @@ public abstract class User {
 
         //Initialise synchronization between allAdmins and Firebase
         ref = fire.getReference("admins");
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot child: snapshot.getChildren()){
-                    Admin admin = child.getValue(Admin.class);
-                    if (!allAdmins.contains(admin)){
-                        allAdmins.add(admin);
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey){
+                Admin admin = dataSnapshot.getValue(Admin.class);
+                if (!allAdmins.contains(admin)){
+                    allAdmins.add(admin);
+                }
+                Log.d("test", "Found admin: " + admin.username);
+//                Log.d("test", customer.fullName);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+                Admin admin = dataSnapshot.getValue(Admin.class);
+                for (User a : User.fetchAllAdmins()) {
+                    if (a.username == admin.username) {
+                        Admin ac = (Admin)a;
+                        ac.venues = admin.venues;
+                        ac.password = admin.password;
+                        break;
                     }
-                    Log.d("test", admin.username);
                 }
             }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Admin admin = dataSnapshot.getValue(Admin.class);
+                allAdmins.remove(admin);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -103,43 +153,100 @@ public abstract class User {
 
         //Initialise synchronization between allEvents and Firebase
         ref = fire.getReference("events");
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot child: snapshot.getChildren()){
-                    Event event = child.getValue(Event.class);
-                    if (!allEvents.contains(event)){
-                        allEvents.add(event);
-                    }
-                    Log.d("test", event.eventID);
-                    Log.d("test",event.customers.toString());
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey){
+                Event event = dataSnapshot.getValue(Event.class);
+                if (!allEvents.contains(event)){
+                    allEvents.add(event);
                 }
+                Log.d("test", "Found event: " + event.eventID);
+//                Log.d("test", customer.fullName);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+                Event event = dataSnapshot.getValue(Event.class);
+                for (Event e : User.fetchAllEvents()) {
+                    if (e.eventID == event.eventID) {
+                        e.startTime = event.startTime;
+                        e.venueID = event.venueID;
+                        e.endTime = event.endTime;
+                        e.capacity = event.capacity;
+                        e.creator = event.creator;
+                        e.customers = event.customers;
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Event event = dataSnapshot.getValue(Event.class);
+                allEvents.remove(event);
             }
 
             @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("test",error.toString());
+                Log.d("test", error.toString());
             }
         });
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot child: snapshot.getChildren()){
+//                    Event event = child.getValue(Event.class);
+//                    if (!allEvents.contains(event)){
+//                        allEvents.add(event);
+//                    }
+//                    Log.d("test", event.eventID);
+//                    Log.d("test",event.customers.toString());
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Log.d("test",error.toString());
+//            }
+//        });
 
         //Initialise synchronization between allVenues and Firebase
         ref = fire.getReference("venues");
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot child: snapshot.getChildren()){
-                    Venue venue = child.getValue(Venue.class);
-                    if (!allVenues.contains(venue)){
-                        allVenues.add(venue);
-                    }
-                    Log.d("test", venue.venueID);
-                    Log.d("test",venue.events.toString());
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey){
+                Venue venue = dataSnapshot.getValue(Venue.class);
+                if (!allVenues.contains(venue)){
+                    allVenues.add(venue);
                 }
+                Log.d("test", "Found venue: " + venue.venueName);
+//                Log.d("test", customer.fullName);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+                Venue venue = dataSnapshot.getValue(Venue.class);
+                for (Venue v : User.fetchAllVenues()) {
+                    if (v.venueID == venue.venueID) {
+                        v.venueName = venue.venueName;
+                        v.venueLocation = venue.venueLocation;
+                        v.events = venue.events;
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Venue venue = dataSnapshot.getValue(Venue.class);
+                allVenues.remove(venue);
             }
 
             @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("test",error.toString());
+                Log.d("test", error.toString());
             }
         });
     }
