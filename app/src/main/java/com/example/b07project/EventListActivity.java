4c0f6c20f4d0f98832f71ac0acd32e3ca2aac4c2
Mudit Jain <mjain@Mudits-MacBook-Pro.local>
2022-08-02 14:42:29 -0400

    Refactoring event list activity to be simpler

--- app/src/main/java/com/example/b07project/EventListActivity.java
+++ app/src/main/java/com/example/b07project/EventListActivity.java
@@ -2,7 +2,14 @@ package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 
 public class EventListActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class EventListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        Intent intent = getIntent();
+        Customer customer = intent.getParcelableExtra("Customer");
+
+        ArrayList<Event> allEvents = customer.fetchAllEvents();
+
     }
 }
