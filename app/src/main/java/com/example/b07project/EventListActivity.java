20bc0a08e31532e1882f3e914a71d24d3d01302e
Mudit Jain <mjain@Mudits-MacBook-Pro.local>
2022-08-02 17:34:23 -0400

    Integrated EventsRecyclerAdapter

--- app/src/main/java/com/example/b07project/EventListActivity.java
+++ app/src/main/java/com/example/b07project/EventListActivity.java
@@ -1,6 +1,8 @@
 package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,16 +14,24 @@ import java.util.ArrayList;
 
 
 public class EventListActivity extends AppCompatActivity {
+    private ArrayList<Event> allEvents;
+    private ArrayList<Venue> allVenues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
 
+        RecyclerView eventsRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        
+        //get event info
         Intent intent = getIntent();
         Customer customer = intent.getParcelableExtra("Customer");
+        allEvents = customer.fetchAllEvents();
+        allVenues = customer.fetchAllVenues();
 
-        ArrayList<Event> allEvents = customer.fetchAllEvents();
-
+        EventsRecyclerAdapter adapter = new EventsRecyclerAdapter(allEvents, allVenues);
+        eventsRecyclerView.setAdapter(adapter);
+        eventsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
