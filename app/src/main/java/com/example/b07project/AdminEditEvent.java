5a6699b818263126803b66e892db1aeb2679ec4c
Mudit Jain <Jain.m3392@gmail.com>
2022-08-07 15:56:36 -0400

    Initial commit

--- app/src/main/java/com/example/b07project/AdminEditEvent.java
+++ app/src/main/java/com/example/b07project/AdminEditEvent.java
@@ -1,2 +1,14 @@
-package com.example.b07project;public class AdminEditEvent {
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminEditEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edit_event);
+    }
 }
