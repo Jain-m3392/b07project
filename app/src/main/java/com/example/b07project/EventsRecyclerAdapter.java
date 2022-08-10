b7c442f6651c70923891c3983d9686c658a8d03b
Mudit Jain <Jain.m3392@gmail.com>
2022-08-10 09:20:55 -0400

    Removed unused imports

--- app/src/main/java/com/example/b07project/EventsRecyclerAdapter.java
+++ app/src/main/java/com/example/b07project/EventsRecyclerAdapter.java
@@ -8,9 +8,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.database.DatabaseReference;
-
 import java.util.ArrayList;
 
 public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAdapter.ViewHolder> {
